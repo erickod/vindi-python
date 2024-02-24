6860dc84767b06106b74a98757c34eb315a0bcb0
Erick Duarte <erickod@gmail.com>
2024-02-24 07:34:43 -0300

    fix: wrong environment typing

--- vindi/client.py
+++ vindi/client.py
@@ -1,7 +1,8 @@
+from typing import Literal
 from .config import Config
 
 class Client:
-    def __init__(self, api_key: str, environment:str = "sandbox") -> None:
+    def __init__(self, api_key: str, environment:Literal["prod", "sandbox"] = "sandbox") -> None:
         self._config = Config(api_key=api_key, environment = environment)
 
     @property
