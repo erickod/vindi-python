58a82bc6404e17b66becb3882ef2d1664353bddb
Erick Duarte <erickod@gmail.com>
2024-02-24 07:09:39 -0300

    test: add tests config.api_key and config.environment

---
+++ tests/test_config.py
@@ -0,0 +1,11 @@
+from vindi.config import Config
+import pytest
+
+async def test_ensure_config_raises_when_api_key_is_an_invalid_value() -> None:
+    with pytest.raises(ValueError):
+        Config(api_key=10000, environment="prod")
+
+@pytest.mark.parametrize("environ", [10000, "any invalid str"])
+async def test_ensure_config_raises_when_environment_is_an_invalid_value(environ) -> None:
+    with pytest.raises(ValueError):
+        Config(api_key="any_valid_api_key", environment=environ)
--- vindi/config.py
+++ vindi/config.py
@@ -1,6 +1,16 @@
 from dataclasses import dataclass
+from typing import Literal
 
-@dataclass
+@dataclass(frozen=True)
 class Config:
     api_key: str
-    environment: str
+    environment: Literal["sandbox", "prod"]
+
+    def __post_init__(self) -> None:
+        ALLOWED_ENVIRONMENTS = ("prod", "sandbox")
+        if not isinstance(self.api_key, str):
+            raise ValueError(f"api key must be a str, got {type(self.api_key)}")
+        if not isinstance(self.environment, str):
+            raise ValueError(f"environment must be a str, got {type(self.environment)}")
+        if self.environment not in ALLOWED_ENVIRONMENTS:
+            raise ValueError(f"environment {self.environment} doenst exist. The options are {ALLOWED_ENVIRONMENTS}")
