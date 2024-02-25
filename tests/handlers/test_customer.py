c7c270a1c47bd1948bfb82fc1cc076f58acf37ac
Erick Duarte <erickod@gmail.com>
2024-02-25 15:20:32 -0300

    refactor: split Customer and CustomerHandler tests

---
+++ tests/handlers/test_customer.py
@@ -0,0 +1,27 @@
+from vindi.handlers.customer import Address, Customer
+
+
+address = Address(
+    street="Rua dos bobos",
+    neighborhood="bairro ABC",
+    city="city",
+    zipcode="72120020",
+    complement="",
+    number="1",
+    country="BR",
+)
+
+
+async def test_customer_is_instantiated_with_right_params() -> None:
+    sut = Customer(
+        "John Doe",
+        "mail@mail.com",
+        "04071239468",
+        code="EXTERNAL_API_CODE",
+        address=address,
+    )
+    assert sut.name == "John Doe"
+    assert sut.email == "mail@mail.com"
+    assert sut.documentation == "04071239468"
+    assert sut.code == "EXTERNAL_API_CODE"
+    assert sut.address == address
--- tests/handlers/test_customer_handler.py
+++ tests/handlers/test_customer_handler.py
@@ -16,21 +16,6 @@ address = Address(
 )
 
 
-async def test_customer_is_instantiated_with_right_params() -> None:
-    sut = Customer(
-        "John Doe",
-        "mail@mail.com",
-        "04071239468",
-        code="EXTERNAL_API_CODE",
-        address=address,
-    )
-    assert sut.name == "John Doe"
-    assert sut.email == "mail@mail.com"
-    assert sut.documentation == "04071239468"
-    assert sut.code == "EXTERNAL_API_CODE"
-    assert sut.address == address
-
-
 async def test_ensure_create_customer_calls_the_http_client_method() -> None:
     http_client = FakeHttpClient()
     sut = CustomerHandler(
