23d629334b9b8165cd256c396cb554f667f44750
Erick Duarte <erickod@gmail.com>
2024-03-12 09:09:32 -0300

    fix: add state field to the address

--- vindi/address.py
+++ vindi/address.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 class Address:
     street: str
     neighborhood: str
+    state: str
     city: str
     zipcode: str
     complement: str
@@ -14,6 +15,7 @@ class Address:
     def asdict(self) -> dict[str, str]:
         return {
             "street": self.street,
+            "state": self.state,
             "neighborhood": self.neighborhood,
             "city": self.city,
             "zipcode": self.zipcode,
